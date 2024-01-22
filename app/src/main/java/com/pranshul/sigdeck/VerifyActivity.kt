ce68c40bf89f764c857ff1b8a4d5a82bc3dbc59a
Realaoleep <10957870+Realaoleep@users.noreply.github.com>
2024-01-22 07:27:00 +0000

    sign activity: fix key storage handling

---
+++ app/src/main/java/com/pranshul/sigdeck/VerifyActivity.kt
@@ -0,0 +1,7 @@
+package com.pranshul.sigdeck
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class VerifyActivity : AppCompatActivity() {
