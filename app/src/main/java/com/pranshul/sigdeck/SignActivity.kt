31117af6bbcd90fe30382dc8169d86e67340a42b
Pranshul <10957870+Realaoleep@users.noreply.github.com>
2024-01-22 04:11:00 +0000

    qr payloads: tidy verification output

---
+++ app/src/main/java/com/pranshul/sigdeck/SignActivity.kt
@@ -0,0 +1,12 @@
+package com.pranshul.sigdeck
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.io.File
+
+class SignActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign)
