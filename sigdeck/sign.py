7c4bd7d18cde2d086eaffb4334555edf392966f4
Realaoleep <10957870+Realaoleep@users.noreply.github.com>
2025-12-27 10:22:00 +0000

    scrypt wrapper: add key export

--- sigdeck/sign.py
+++ sigdeck/sign.py
@@ -8,3 +8,9 @@ def sign_bytes(message, seed):
 
 
 def sign_file(path, seed):
+    data = path.read_bytes()
+    return sign_bytes(data, seed)
+
+
+def detached_path(path):
+    return path.with_suffix(str(path.suffix) + ".sig")
